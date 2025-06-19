0d2b4f1cb27418ccb3719a35770d08df6ce39f2c
mehdad-hussain <hussain.mehdad1@gmail.com>
2025-06-19 23:57:29 +0600

    [chore]: installed tailwind 4 and shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { Button } from '@/components/ui/button'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [data, setData] = useState<any>(null)
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -14,13 +15,16 @@ function App() {
         console.error('Error fetching data:', error)
       }
     }
-    
+
     fetchData()
   }, [])
 
   return (
     <div>
-      <h1>Display the data obtained from API here</h1>
+      <h1 className="text-2xl font-bold  text-red-500 mb-4">
+        Display the data obtained from API here
+      </h1>
+      <Button>Click me</Button>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
